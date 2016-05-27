00edee33da2b7aa50a9a91579b39350902061371
wscqs <935966764@qq.com>
2016-05-27 18:13:38 +0800

    初始化微博

---
+++ QSWB/QSWB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QSWB
+//
+//  Created by mba on 16/5/27.
+//  Copyright © 2016年 mbalib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
