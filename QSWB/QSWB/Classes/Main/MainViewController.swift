fd42026e0b33e6c8c44f68e3c43ceb5a2f8a8cef
wscqs <935966764@qq.com>
2016-05-28 00:06:46 +0800

    tabbar 主页编写

--- QSWB/QSWB/Classes/Main/MainViewController.swift
+++ QSWB/QSWB/Classes/Main/MainViewController.swift
@@ -12,24 +12,23 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - 增加子视图
+    private func addChildViewController(childController: UIViewController, title: String, imageName: String) {
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        childController.title = title
+        let nav = UINavigationController()
+        nav.addChildViewController(childController)
+        addChildViewController(nav)
     }
-    */
 
 }
